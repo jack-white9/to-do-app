31af3a092e945ced65a6b70b921470ab66428dc5
Jack White <jfw793@gmail.com>
2021-08-19 13:44:01 +1000

    Moved css import to UIhandler

--- src/UIhandler.js
+++ src/UIhandler.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 export default function UIhandler() {
     //wrapper contents
     const wrapper = document.createElement('wrapper');
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import './style.css';
 import UIhandler from './UIhandler.js';
 
 

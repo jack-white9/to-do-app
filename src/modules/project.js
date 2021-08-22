0b29c7c83aaab7d2fc31b60aedde73715e093b2f
Jack White <jfw793@gmail.com>
2021-08-22 22:15:59 +1000

    Tidied project file

--- src/modules/project.js
+++ src/modules/project.js
@@ -3,6 +3,5 @@ import TodoList from './taskList.js'
 export default class Project extends TodoList {
     constructor(name) {
         this.name = name;
-        // perhaps todolist should be a child of the project class, inheriting the task objects from the project. idrk.
     }
 }

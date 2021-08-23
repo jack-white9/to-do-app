64e563c7cb50a01761587687d9161495d3dd80f7
Jack White <jfw793@gmail.com>
2021-08-23 17:53:29 +1000

    Linted js files (Airbnb style)

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,54 +1,48 @@
 import '../style.css';
-import Project from './project'
+import Project from './project';
 import ProjectList from './projectList';
-import Task from './task';
 
 export default function UI() {
-    // Skeleton HTML
-    const wrapper = document.createElement('wrapper');
-    wrapper.innerHTML += `
-        <container class="projectContainer">
-            <h1 class="projectHeader">Projects</h1>
-            <hr class="projectDivider"></hr>
-            <container class="projectListContainer"></container>
-            <p class="addProject">+ Add Project</p>
+// Skeleton HTML
+  const wrapper = document.createElement('wrapper');
+  wrapper.innerHTML += `
+    <container class="projectContainer">
+        <h1 class="projectHeader">Projects</h1>
+        <hr class="projectDivider"></hr>
+        <container class="projectListContainer"></container>
+        <p class="addProject">+ Add Project</p>
+    </container>
+    <container class="tasksContainer">
+        <container class="tasksHeaderContainer">
+            <h1 class="tasksHeader">Chores</h1>
+            <p class="edit">Edit</p>
         </container>
-        <container class="tasksContainer">
-            <container class="tasksHeaderContainer">
-                <h1 class="tasksHeader">Chores</h1>
-                <p class="edit">Edit</p>
-            </container>
-            <hr class="tasksDivider"></hr>
-            <h2 class="tasksSubheader"><i>Click a task to check it off.</i></h2>
-            <container class="todoListContainer"></container>
-            <p class="addTask">+ Add Task</p>
-        </container>
-    `
-    document.body.appendChild(wrapper);
+        <hr class="tasksDivider"></hr>
+        <h2 class="tasksSubheader"><i>Click a task to check it off.</i></h2>
+        <container class="todoListContainer"></container>
+        <p class="addTask">+ Add Task</p>
+    </container>
+`;
+  document.body.appendChild(wrapper);
 
-    // Example projects
-    let projectList = new ProjectList;
-    const projectListContainer = document.querySelector('.projectListContainer');
-    let projects = projectList.getProjects().map(function (project) {
-        return `
-        <container class="projectItemContainer">
-            <p class="projects">${project.name}</p>
-            <p class="deleteProject">×</p> 
-        </container>`
-    }).join('');
-    projectListContainer.innerHTML += projects;
+  // Example projects
+  const projectList = new ProjectList();
+  const projectListContainer = document.querySelector('.projectListContainer');
+  const projects = projectList.getProjects().map((project) => `
+    <container class="projectItemContainer">
+        <p class="projects">${project.name}</p>
+        <p class="deleteProject">×</p> 
+    </container>`).join('');
+  projectListContainer.innerHTML += projects;
 
-
-    // Example project contents
-    let chores = new Project('Chores');
-    const todoListContainer = document.querySelector('.todoListContainer');
-    let taskList = chores.getTasks().map(function (task) {
-        return `
-        <container class="todoContainer">
-            <span class="circle"></span>
-            <p class="todoText">${task.name}</p>
-            <p class="delete">×</p> 
-        </container>`
-    }).join('');
-    todoListContainer.innerHTML += taskList;    
+  // Example project contents
+  const chores = new Project('Chores');
+  const todoListContainer = document.querySelector('.todoListContainer');
+  const taskList = chores.getTasks().map((task) => `
+    <container class="todoContainer">
+        <span class="circle"></span>
+        <p class="todoText">${task.name}</p>
+        <p class="delete">×</p> 
+    </container>`).join('');
+  todoListContainer.innerHTML += taskList;
 }
--- src/modules/project.js
+++ src/modules/project.js
@@ -1,30 +1,30 @@
-import Task from './task'
+import Task from './task';
 
 export default class Project {
-    constructor(name) {
-        this.name = name;
-        this.tasks = [];
-        this.tasks.push(new Task('Vacuum the floor', 'unchecked'));
-        this.tasks.push(new Task('Do the dishes', 'unchecked'));
-        this.tasks.push(new Task('Fold the washing', 'unchecked'));
-    }
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+    this.tasks.push(new Task('Vacuum the floor', 'unchecked'));
+    this.tasks.push(new Task('Do the dishes', 'unchecked'));
+    this.tasks.push(new Task('Fold the washing', 'unchecked'));
+  }
 
-    setTasks(tasks) {
-        this.tasks = tasks;
-    }
+  setTasks(tasks) {
+    this.tasks = tasks;
+  }
 
-    getTasks() {
-        return this.tasks;
-    }
+  getTasks() {
+    return this.tasks;
+  }
 
-    addTask(taskName, status) {
-        this.tasks.push(new Task(taskName, status));
-    }
+  addTask(taskName, status) {
+    this.tasks.push(new Task(taskName, status));
+  }
 
-    deleteTask(taskName) {
-        const index = this.tasks.indexOf(taskName);
-        if (index > -1) {
-            this.tasks.splice(index, 1);
-        }
+  deleteTask(taskName) {
+    const index = this.tasks.indexOf(taskName);
+    if (index > -1) {
+      this.tasks.splice(index, 1);
     }
+  }
 }

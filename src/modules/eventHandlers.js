4337ee1630aa6c0005d0d23194b9c698ddf9a1fe
Jack White <jfw793@gmail.com>
2021-08-23 13:39:16 +1000

    Implemented delete button for tasks

--- src/modules/eventHandlers.js
+++ src/modules/eventHandlers.js
@@ -9,8 +9,15 @@ export default function eventHandlers() {
     const projectListContainer = document.querySelector('.projectListContainer');
     const taskListContainer = document.querySelector('.todoListContainer');
     let taskHeader = document.querySelector('.tasksHeader');
+    let selectedProject = taskHeader.innerHTML;
 
     updateProjects();
+    updateTasks(selectedProject);
+
+
+    // consider: let selectedProject = taskHeader.innerHTML;
+    // then change the value in the same way at the end of updateProject(), then updateProject() can track changes and reduce
+    // clunkiness in functions and allow starting function call
 
     // Add project
     addProject.addEventListener('click', () => {
@@ -24,6 +31,7 @@ export default function eventHandlers() {
             if (event.key === 'Enter' && projectInput.value != '') {
                 projectList.addProject(projectInput.value);
                 updateProjects();
+                //deleteTaskClickListener();
             } // add condition to reject projects with duplicate names
         });
     });
@@ -65,8 +73,8 @@ export default function eventHandlers() {
 
     // Function to cycle through task array and update DOM
     function updateTasks(project) {
-        let taskArray = projectList.getProjects().filter(object => object.name === project);
-        let tasks = taskArray[0].getTasks().map(function (task) {
+        let currentProject = projectList.getProjects().filter(object => object.name === project)[0];
+        let tasks = currentProject.getTasks().map(function (task) {
             return `
             <container class="todoContainer">
             <span class="circle"></span>
@@ -76,6 +84,8 @@ export default function eventHandlers() {
         }).join('');
         taskListContainer.innerHTML = tasks;
         taskHeader.innerHTML = project;
+        selectedProject = taskHeader.innerHTML;
+        deleteTaskClickListener();
     }
 
     // Add task
@@ -92,13 +102,28 @@ export default function eventHandlers() {
             if (event.key === 'Enter' && taskInput.value != '') {
                 let currentProject = projectList.getProjects().filter(object => object.name === taskHeader.innerHTML);
                 currentProject[0].addTask(taskInput.value, 'unchecked');
-                console.log(currentProject[0].getTasks())
                 updateTasks(taskHeader.innerHTML);
             } // add condition to reject tasks with duplicate names
         });
     })
 
     // Remove task
+    function deleteTaskClickListener() {
+        const deleteTask = document.querySelectorAll('.delete');
+        deleteTask.forEach(button => {
+            button.addEventListener('click', () => {
+                let currentProject = projectList.getProjects().filter(project => project.name === selectedProject)[0];
+                console.log(currentProject);
+                let selectedTask = currentProject.getTasks().filter(object => object.name === button.parentNode.children[1].innerHTML)[0];
+                console.log(selectedTask)
+                projectList.getProjects()[0].deleteTask(selectedTask);
+                console.log(projectList.getProjects()) // returns old project too for some reason
+                updateTasks(taskHeader.innerHTML);
+            });
+        });
+    }
 
     // Check/uncheck task
+    
+
 }
